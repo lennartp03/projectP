48f98e5995180f923a5984ea949975638e9063fe
PhSchumi <philipp.a.schumacher@web.de>
2025-02-10 09:49:57 +0100

    '--'

--- src/features_monthly.py
+++ src/features_monthly.py
@@ -62,4 +62,196 @@ def calculate_anomalies(df, reference_period_df):
     # calculate climate anomalies
     grouped_anomaly = df.copy()
     for column in columns:
-        grouped_anomaly[colum
+        grouped_anomaly[column] = (df[column] - rp_grouped[column].mean()) / rp_grouped[column].std()
+    return grouped_anomaly
+
+
+
+
+def plot_monthly(df, columns, titles, deg):
+    # only keep values from years 1996, 2000, 2004, 2008, 2012, 2016
+    df = df[df['year'].isin([1996,  2003,  2010, 2017])]
+
+
+    for column in columns:
+        plt.figure(figsize=(12, 3))
+        
+        # for each column, use only rows that dont have zero values
+        df = df[df[column] != 0]
+
+        # get title from titles dictionary
+        title = titles[column]
+
+        #scatter = plt.scatter(df['month'], df[column], c = 'black')
+        scatter = plt.scatter(df['month'], df[column], 
+                          c=df['year'], cmap='viridis')
+    
+        # Add colorbar
+        plt.colorbar(scatter, label='Year')
+        plt.xlabel('month')
+        plt.title(title)
+        plt.tight_layout()
+        plt.show()
+
+
+def run_temperature_regression(df, columns):
+    df['sin_month'] = np.sin(2 * np.pi * df['month'] / 12)
+    df['cos_month'] = np.cos(2 * np.pi * df['month'] / 12)
+    df['year'] = df['year'] - 1996
+    df['year'] = df['year'] + 1
+
+    for column in columns:
+        X = df[['year', 'sin_month', 'cos_month']]
+        y = df[column]
+        X = sm.add_constant(X)
+        model = sm.OLS(y, X).fit()
+        print(model.summary())
+
+def monthly_stats(df, columns):
+    """
+    Mean and standard deviation grouped by month
+    """
+    results = {"Month": [], "Mean": [], "Std": []}
+    for column in columns:
+        monthly = df.groupby("month")[column].agg(["mean", "std"])
+        results["Month"] = monthly.index
+        results["Mean"] = monthly["mean"]
+        results["Std"] = monthly["std"]
+    results_df = pd.DataFrame(results)
+    return results_df
+
+def monthly_stats_each_year(df, columns):
+    results = {"Year": [], "Month": [], "Mean": []}
+    for column in columns:
+        for year in df["year"].unique():
+            monthly = df[df["year"] == year].groupby("month")[column].agg(["mean"])
+            results["Year"].extend([year] * 12)
+            results["Month"].extend(monthly.index)
+            results["Mean"].extend(monthly["mean"])
+    results_df = pd.DataFrame(results)
+    return results_df
+
+
+def anomaly_detection(df, columns, threshold):
+    results = {"Variable": [], "Year": [], "Month": [], "Z-Score": []}
+    for column in columns:
+        df['Z-Score'] = zscore(df[column])
+
+        # Define extreme anomalies (e.g., beyond ±2 std deviations)
+        anomalies = df[np.abs(df['Z-Score']) > threshold].reset_index(drop=True)
+        results["Variable"].extend([column] * len(anomalies))
+        results["Year"].extend(anomalies["year"])
+        results["Month"].extend(anomalies["month"])
+        results["Z-Score"].extend(anomalies["Z-Score"])
+    results_df = pd.DataFrame(results)
+
+    # print number of anomalies
+    print(f'Number of anomalies: {len(results_df)}')
+
+def rolling_mean(df, columns):
+    results = {"Variable": [], "Year": [], "Month": [], "Rolling_Mean": []}
+    for column in columns:
+        df["Rolling_Mean"] = df[column].rolling(window=12, center=True).mean()
+        #df.plot(x="time", y=[column, "Rolling_Mean"], figsize=(10, 5), alpha=0.7)
+        results["Variable"].extend([column] * len(df))
+        results["Year"].extend(df["year"])
+        results["Month"].extend(df["month"])
+        results["Rolling_Mean"].extend(df["Rolling_Mean"])
+    results_df = pd.DataFrame(results)
+    return results_df
+
+def describe_data(df, columns):
+    results = {"Variable": [], "Mean": [], "Std": [], "Min": [], "25%": [], "50%": [], "75%": [], "Max": []}
+    for column in columns:
+        results["Variable"].append(column)
+        results["Mean"].append(df[column].mean())
+        results["Std"].append(df[column].std())
+        results["Min"].append(df[column].min())
+        results["25%"].append(df[column].quantile(0.25))
+        results["50%"].append(df[column].median())
+        results["75%"].append(df[column].quantile(0.75))
+        results["Max"].append(df[column].max())
+    results_df = pd.DataFrame(results)
+    return results_df
+
+def interquartile_range(df, columns):
+    results = {"Variable": [], "IQR": []}
+    for column in columns:
+        q1 = df[column].quantile(0.25)
+        q3 = df[column].quantile(0.75)
+        iqr = q3 - q1
+        results["Variable"].append(column)
+        results["IQR"].append(iqr)
+    results_df = pd.DataFrame(results)
+    return results_df
+
+def percentile_analysis(df, columns):
+    results = {"Variable": [], "5th Percentile": [], "95th Percentile": [], "Count Below": [], "Count Above": []}
+    for col in columns:
+        lower_bound = grouped_anomaly[col].quantile(0.05)
+        upper_bound = grouped_anomaly[col].quantile(0.95)
+        count_lower = (grouped_anomaly[col] < lower_bound).sum()
+        count_upper = (grouped_anomaly[col] > upper_bound).sum()
+        results["Variable"].append(col)
+        results["5th Percentile"].append(lower_bound)
+        results["95th Percentile"].append(upper_bound)
+        results["Count Below"].append(count_lower)
+        results["Count Above"].append(count_upper)
+        print(f"Variable: {col}")
+        print(f"5th Percentile: {lower_bound}")
+        print(f"95th Percentile: {upper_bound}")
+        print(f"Count Below: {count_lower}")
+        print(f"Count Above: {count_upper}")
+        
+    results_df = pd.DataFrame(results) 
+    return results_df
+
+def decompose_time_series(df, columns):
+    # Ensure you have a proper DateTime index:
+    result_dict = {"Variable": [], "Trend": [], "Seasonal": [], "Residual": []}
+    for column in columns:
+        df['date'] = pd.to_datetime(df[['year', 'month']].assign(Day=1))
+        df.set_index('date', inplace=True)
+
+        # Choose a variable, for example, sea surface temperature:
+
+        # Decompose with an assumed period of 12 months (adjust if needed)
+        stl = STL(df[column].dropna(), period=12)
+        result = stl.fit()
+        # Plot the decomposed components:
+        result.plot()
+        # plt.suptitle(f"STL Decomposition of {column}", fontsize=14)
+        plt.show()
+        # Store the results in a dictionary:
+        result_dict["Variable"].append(column)
+        result_dict["Trend"].append(result.trend)
+        result_dict["Seasonal"].append(result.seasonal)
+        result_dict["Residual"].append(result.resid)
+
+    result_df = pd.DataFrame(result_dict)
+    return result_df
+
+
+
+
+def plot_yearly(df, columns, titles, deg):
+    for column in columns:   
+        plt.figure(figsize=(12, 3))
+        
+        # for each column, use only rows that dont have zero values
+        df = df[df[column] != 0]
+
+        title = titles[column]
+
+        plt.scatter(df['year'], df[column], c = 'black')
+
+        # add regression line
+        z = np.polyfit(df['year'], df[column], deg)   # Hier 1, 2, 3, oder 4 degs rein?
+        p = np.poly1d(z)
+        plt.plot(df['year'],p(df['year']), linestyle = "-", c = 'red', linewidth = 0.3)
+
+
+        plt.xlabel('Year')
+        plt.title(title)
+        plt.tight_layout()
+        plt.show()
--- src/features_penguins.py
+++ src/features_penguins.py
@@ -85,4 +85,262 @@ def direction_of_travel(df):
         if df.at[i, 'track_id'] != df.at[i-1,'track_id']:
             df.at[i, 'direction_of_travel'] = df.at[i, 'relative_position']
         else:
-            delta_lat 
+            delta_lat = delta_lat = df.iloc[i]['latitude'] - df.iloc[i-1]['latitude']
+            delta_lon = df.iloc[i]['longitude'] - df.iloc[i-1]['longitude']
+            degrees_temp = math.atan2(delta_lat, delta_lon)/math.pi*180
+
+            if degrees_temp < 0:
+                degrees_final = degrees_temp + 360
+            else:
+                degrees_final = degrees_temp
+
+            lookup = round(degrees_final/45)
+
+            df.at[i, 'direction_of_travel'] = directions[lookup]
+    return df
+
+
+def distance_since_last_measure(df):
+    """
+    Distance travelled between each measure 
+    """
+    df['km_since_last_measure'] = 0
+    df['km_since_last_measure'] = df['km_since_last_measure'].astype(float)
+    for i in range(1, len(df)):
+        if df.at[i, 'track_id'] == df.at[i-1, 'track_id']:
+            df.at[i, 'km_since_last_measure'] = distance.distance(df.at[i,'pos_bird'], df.at[i-1,'pos_bird']).km
+        else:
+            df.at[i, 'km_since_last_measure'] = 0
+    return df
+
+
+
+
+def distance_direction(df):
+    """
+    Distace North, East, South, West
+    """
+    df['delta_km_north'] = 0
+    df['delta_km_north'] = df['delta_km_north'].astype(float)
+    df['delta_km_south'] = 0
+    df['delta_km_south'] = df['delta_km_south'].astype(float)
+
+    for i in range(1, len(df)):
+        start = (0, df.at[i-1, 'longitude'])
+        finish = (0, df.at[i, 'longitude'])
+        delta = distance.distance(start, finish).km
+        if df.at[i, 'track_id'] == df.at[i-1, 'track_id']:
+            if delta > 0:
+                df.at[i, 'delta_km_north'] = delta
+            if delta < 0:
+                df.at[i, 'delta_km_south'] = delta
+        else:
+            df.at[i, 'delta_km_north'] = 0
+            
+
+
+    df['delta_km_east'] = 0
+    df['delta_km_east'] = df['delta_km_east'].astype(float)
+    df['delta_km_west'] = 0
+    df['delta_km_west'] = df['delta_km_west'].astype(float)
+
+    for i in range(1, len(df)):
+        start = (df.at[i-1, 'latitude'], 0)
+        finish = (df.at[i, 'latitude'], 0)
+        delta = distance.distance(start, finish).km
+        if df.at[i, 'track_id'] == df.at[i-1, 'track_id']:
+            if delta > 0:
+                df.at[i, 'delta_km_east'] = delta
+            elif delta < 0:
+                df.at[i, 'delta_km_west'] = delta
+        else:
+            df.at[i, 'delta_km_east'] = 0
+
+    return df
+
+
+def time_difference(df):
+    df['minutes_since_last_measure'] = 0
+    df['minutes_since_last_measure'] = df['minutes_since_last_measure'].astype(float)
+    for i in range(1, len(df)):
+        if df.at[i, 'track_id'] == df.at[i-1, 'track_id']:
+            timestamp_1 = df.at[i-1, 'datetime']
+            timestamp_2 = df.at[i, 'datetime']
+            df.at[i,'minutes_since_last_measure'] = (timestamp_2 - timestamp_1).total_seconds()/60
+        else:
+            df.at[i, 'minutes_since_last_measure'] = 0
+    return df
+
+
+def save_df(df, output_path, name=str):
+    df.to_csv(output_path + '/' +'penguins_cleaned.zip', compression={'method': 'zip', 'archive_name': name}, index=False)
+
+
+def average_per_day(df):
+    """
+    Average time stamps to match weather: One per day
+    Try to get a distribution for those accumulated time stamps --> one for each day per unique_id
+    """
+    df['date_gmt'] = pd.to_datetime(df['date_gmt'], format='%d/%m/%Y')
+
+    aggregate_cols = ['latitude', 'longitude', 'lat_colony', 'lon_colony', 'km_to_colony', 'km_since_last_measure', 
+                        'delta_km_north', 'delta_km_south', 'delta_km_east', 
+                        'delta_km_west', 'minutes_since_last_measure']
+
+    # Calculate mean
+    df_mean = df.groupby(['track_id', 'date_gmt'])[aggregate_cols].mean().reset_index()
+    df_mean.columns = ['track_id', 'date_gmt'] + [f'{col}_mean' for col in aggregate_cols]
+
+    # Calculate standard deviation
+    df_std = df.groupby(['track_id', 'date_gmt'])[aggregate_cols].std(ddof=0).reset_index()
+    df_std.columns = ['track_id', 'date_gmt'] + [f'{col}_std' for col in aggregate_cols]
+
+    #df_min
+    df_min = df.groupby(['track_id', 'date_gmt'])[aggregate_cols].min().reset_index()
+    df_min.columns = ['track_id', 'date_gmt'] + [f'{col}_min' for col in aggregate_cols]
+
+    #df_max
+    df_max = df.groupby(['track_id', 'date_gmt'])[aggregate_cols].max().reset_index()
+    df_max.columns = ['track_id', 'date_gmt'] + [f'{col}_max' for col in aggregate_cols]
+
+    # Merge dataframes
+    df_penguins_pt1 = pd.merge(df_mean, df_std, on=['track_id', 'date_gmt'])
+
+    df_penguins_pt2 = pd.merge(df_min, df_max, on=['track_id', 'date_gmt'])
+
+    df_penguins_final = pd.merge(df_penguins_pt1, df_penguins_pt2, on=['track_id', 'date_gmt'])
+
+    # Lookup operation to add 'common_name', 'site_name','colony_name' back based on track id
+    df_subset = df.drop_duplicates(subset='track_id')
+    add_back = ['common_name', 'site_name','colony_name']
+    for column in add_back:
+        df_penguins_final[column] = df_penguins_final['track_id'].map(df_subset.set_index('track_id')[column])
+
+    return df_penguins_final
+
+### ALTERNATIVE APPROACH; MAYBE MORE ELEGANT (IN TESTING) ###
+"""
+def average_per_day(df):
+    # Convert 'date_gmt' to datetime
+    df['date_gmt'] = pd.to_datetime(df['date_gmt'], format='%d/%m/%Y')
+
+    # List of columns to aggregate
+    aggregate_cols = [
+        'latitude', 'longitude', 'lat_colony', 'lon_colony', 'km_to_colony',
+        'km_since_last_measure', 'delta_km_north', 'delta_km_south',
+        'delta_km_east', 'delta_km_west', 'minutes_since_last_measure'
+    ]
+
+    # Create an aggregation dictionary that applies multiple functions to each column
+    agg_funcs = {col: ['mean', 'std', 'min', 'max'] for col in aggregate_cols}
+
+    # Group the DataFrame by 'track_id' and 'date_gmt' and apply the aggregations
+    df_agg = df.groupby(['track_id', 'date_gmt']).agg(agg_funcs)
+
+    # Flatten the multi-level column index
+    df_agg.columns = [f"{col}_{func}" for col, func in df_agg.columns]
+    df_agg = df_agg.reset_index()
+
+    # Create a lookup DataFrame that contains one row per track_id
+    additional_cols = ['common_name', 'site_name', 'colony_name']
+    df_lookup = df.drop_duplicates(subset='track_id').set_index('track_id')
+
+    for col in additional_cols:
+        df_agg[col] = df_agg['track_id'].map(df_lookup[col])
+
+    return df_agg
+
+"""
+
+
+
+def save_to_json(df, path, name=str):
+    """
+    Save unique dates to JSON file to access with API (climate data)
+    """
+    df['date_gmt'] = df['date_gmt'].astype(str)
+
+    # extract each unique combination of year, month and day in a dictionary
+    unique_dates = df['date_gmt'].unique()
+    unique_dates_dict = {}
+
+    # suppose preprocessing of date already happened
+    for date in unique_dates:
+        year = date.split('-')[0]
+        month = date.split('-')[1]
+        day = date.split('-')[2]
+        unique_dates_dict[date] = {'year': year, 'month': month, 'day': day}
+
+
+    #df_encoded.to_csv(path_json + '/' + 'penguins_cleaned.csv', index=False, compression='zip')
+    json.dump(unique_dates_dict, open(path + '/' + name, 'w'))
+    print('JSON file saved to ' + path)
+
+
+
+
+### ======= FUNCTIONS UNSURE IF NEEDED ======= ###
+
+def mean_deviation_colony(df, df_penguins_final):
+    species = df['common_name'].unique()
+    years = df_penguins_final['year'].unique()
+    results_species_colony = []
+    for year in years:
+        for specie in species:
+            for colony in df[df['common_name'] == specie]['colony_name'].unique():
+                df_specie = df_penguins_final[(df_penguins_final['common_name'] == specie) & (df_penguins_final['colony_name'] == colony) & (df_penguins_final['year'] == year)]
+                # Calculate mean deviation manually
+                mean_value = df_specie['km_to_colony_mean'].mean()
+                # mean_deviation = (df_specie['km_to_colony_mean'] - mean_value).abs().mean()
+                results_species_colony.append({'year': year, 'species': specie, 'colony': colony, 'mean distance': mean_value})
+    results_species_colony = pd.DataFrame(results_species_colony)
+    return results_species_colony
+
+
+
+def plot_yearly_species(df, deg, species):
+    df['date_gmt'] = pd.to_datetime(df['date_gmt'])
+    df['year'] = df['date_gmt'].dt.year
+    df['month'] = df['date_gmt'].dt.month
+    for specie in species:
+        plt.figure(figsize=(12, 3))
+        
+        # Filter the dataframe for the current species
+        df_species = df[df['common_name'] == specie].copy()  # Use .copy() to avoid modifying df
+        # Group by year and compute mean of km_to_colony
+        df_species = df_species.groupby('year', as_index=False)['km_to_colony'].mean()
+        title = specie
+
+        plt.scatter(df_species['year'], df_species['km_to_colony'], c='black')
+
+
+        if len(df_species) > deg:  # Ensure enough points for regression
+            z = np.polyfit(df_species['year'], df_species['km_to_colony'], deg)
+            p = np.poly1d(z)
+            plt.plot(df_species['year'], p(df_species['year']), linestyle="-", c='red', linewidth=0.8)
+
+
+        plt.xlabel('Year')
+        plt.title(title)
+        plt.tight_layout()
+        plt.show()
+
+def spread_species(df, species):
+    for specie in species:
+        print(f'\n========{specie}========')
+        for colony in df[df['common_name'] == specie]['colony_name'].unique():
+            df_specie = df[(df['common_name'] == specie) & (df['colony_name'] == colony)]
+            spread = df_specie['km_to_colony_mean'].std(ddof=1)
+            print(colony, spread)
+
+def calculte_mean_deviation(df):
+    species = df['common_name'].unique()
+    for specie in species:
+        print(f'\n========{specie}========')
+        for colony in df[df['common_name'] == specie]['colony_name'].unique():
+            df_specie = df[(df['common_name'] == specie) & (df['colony_name'] == colony)]
+            
+            # Calculate mean deviation manually
+            mean_value = df_specie['km_to_colony_mean'].mean()
+            mean_deviation = (df_specie['km_to_colony_mean'] - mean_value).abs().mean()
+            print(colony, mean_deviation)
